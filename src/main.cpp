b73baf1437a4829d7a365bad81a2c1034c24c32c
glutesha <mail@glutesha.me>
2025-07-15 00:13:12 +0300

    Bar blueprint

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,12 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 
-#define BRIGHTNESS 100
+#define BRIGHTNESS 200
 
 #define PIN 2
 
 #define mw 32
 #define mh 8
-
-static const unsigned char PROGMEM image_Layer_2_bits[] = {0x38,0x44,0x82,0xba,0x82,0x44,0x38};
 
 Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(mw, mh, 
   PIN,
@@ -20,16 +18,19 @@ Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(mw, mh,
 
 void setup() {
   Serial.begin(9600);
-  matrix->begin();
-  matrix->setTextWrap(false);
-  matrix->setBrightness(BRIGHTNESS);
 
-  matrix->setTextColor(0xC0E5);
-  matrix->setTextWrap(false);
-  matrix->setCursor(9, 0);
-  matrix->print("Busy");
+static const unsigned char PROGMEM image_Layer_3_bits[] = {0xf8,0xcc,0x78,0xcc,0xcc,0xcc,0xcc,0xcc,0xcc,0xcc,0xc0,0xcc,0xf8,0xcc,0x78,0x78,0xcc,0xcc,0x0c,0x30,0xcc,0xcc,0xcc,0x30,0xf8,0x78,0x78,0x30};
 
-  matrix->drawBitmap(0, 0, image_Layer_2_bits, 7, 7, 0xC0E5);
+
+// Layer 1
+matrix->setTextColor(0xE0C4);
+matrix->setTextWrap(false);
+matrix->setCursor(5, -7);
+matrix->print("f");
+
+// Layer 3
+matrix->drawBitmap(1, 1, image_Layer_3_bits, 30, 7, 0xE0C4);
+
   matrix->show();
 }
 
