1769e2c0096804a626540565d9e478a91c6b0ede
glutesha <mail@glutesha.me>
2025-07-17 19:39:59 +0300

    Add ArduinoJson

--- src/web/routes/busy/busy.cpp
+++ src/web/routes/busy/busy.cpp
@@ -1,2 +1,12 @@
 #include "busy.h"
 
+void busy(AsyncWebServer &server, Timer &timer){
+server.on("/api/busy", HTTP_POST, [](AsyncWebServerRequest *request){}, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total){
+    String body;
+
+    for (size_t i = 0; i < len; i++) {
+      body += (char)data[i];
+    }
+
+    
+});}
