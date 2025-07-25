b489d0093a7a8fac77922e948f06b555c914a38d
glutesha <mail@glutesha.me>
2025-07-25 20:29:15 +0300

    Pomodoro backend autonomity start

--- front/bitbusy/src/update.ts
+++ front/bitbusy/src/update.ts
@@ -1,18 +1,18 @@
 import { time } from "./timer";
 import { busy } from "./busy";
 import { get } from "svelte/store";
+import { select } from "./select";
 
 export async function updateDevice(){
+    const req = { "busy": get(busy), "time": get(time), "pomodoro": get(select) == 2 }
+
     try{
         const response = await fetch('/api/busy', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                "busy": get(busy),
-                "time": get(time)
-            })
+            body: JSON.stringify(req);
         });
         if (!response.ok) {
             throw new Error('Network response was not ok');
